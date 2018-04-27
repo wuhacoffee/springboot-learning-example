526186411c3a39d94d2b3baffa475c7430a89f5f
Administrator <Administrator@USER-20170715WE>
2018-04-27 22:48:34 +0800

    qwq

--- chapter-1-spring-boot-quickstart/src/main/java/demo/springboot/QuickStartApplication.java
+++ chapter-1-spring-boot-quickstart/src/main/java/demo/springboot/QuickStartApplication.java
@@ -12,5 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuickStartApplication {
     public static void main(String[] args) {
         SpringApplication.run(QuickStartApplication.class, args);
+        System.out.print("test>>>>>>");
     }
 }
